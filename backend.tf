85ee619b8dd01ad265e367278baa7b8e55faf319
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2025-11-03 17:32:02 +0100

    Updated script to check if the resource group exists

--- backend.tf
+++ backend.tf
@@ -1,9 +1,5 @@
-# BACKEND GENERICO - reemplaza los valores entre <> antes de ejecutar `terraform init`
+# BACKEND GENERICO - Los valores se configuran mediante -backend-config en el workflow
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<REPLACE_RESOURCE_GROUP_NAME>"
-    storage_account_name = "<REPLACE_STORAGE_ACCOUNT_NAME>"
-    container_name       = "<REPLACE_CONTAINER_NAME>"
-    key                  = "<REPLACE_KEY.tfstate>"
   }
 }

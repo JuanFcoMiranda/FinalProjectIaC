eac21098b5a67373c6ace884101f0d04526c26ef
Juan Francisco <jfmiranda75@hotmail.com>
2025-11-14 09:04:21 +0100

    Module references fixed (#73)
    This pull request updates the output references in the
    `modules/aks/outputs.tf` file to use the
    `azurerm_kubernetes_cluster.aks` resource instead of
    `azurerm_kubernetes_cluster.this`. This change ensures consistency with
    the resource naming convention and may help prevent confusion or errors
    in module usage.
    
    Resource reference updates:
    
    * Changed all output values to reference
    `azurerm_kubernetes_cluster.aks` instead of
    `azurerm_kubernetes_cluster.this` for outputs such as `id`, `name`,
    `kube_config`, `host`, `cluster_ca_certificate`,
    `identity_principal_id`, `kubelet_identity_object_id`, and
    `key_vault_secrets_provider_identity`.

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,41 +1,41 @@
 output "id" {
   description = "ID of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.this.id
+  value       = azurerm_kubernetes_cluster.aks.id
 }
 
 output "name" {
   description = "Name of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.this.name
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
 output "kube_config" {
   description = "Kubernetes configuration"
-  value       = azurerm_kubernetes_cluster.this.kube_config_raw
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive   = true
 }
 
 output "host" {
   description = "Kubernetes API server host"
-  value       = azurerm_kubernetes_cluster.this.kube_config[0].host
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
 }
 
 output "cluster_ca_certificate" {
   description = "Cluster CA certificate"
-  value       = azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
   sensitive   = true
 }
 
 output "identity_principal_id" {
   description = "Principal ID of the system assigned identity"
-  value       = azurerm_kubernetes_cluster.this.identity[0].principal_id
+  value       = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
 
 output "kubelet_identity_object_id" {
   description = "Object ID of the kubelet identity"
-  value       = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+  value       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 
 output "key_vault_secrets_provider_identity" {
   description = "Identity of the Key Vault Secrets Provider"
-  value       = azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0].object_id
+  value       = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].object_id
 }

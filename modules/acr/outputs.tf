a3ced16fff490fbaa54b83a1815a72c2723d792d
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2025-11-14 09:23:33 +0100

    Module references fixed

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -1,4 +1,4 @@
 output "login_server" {
   description = "The login server of the container registry"
-  value       = azurerm_container_registry.this.login_server
+  value       = azurerm_container_registry.acr.login_server
 }
--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,9 +1,9 @@
 output "name" {
   description = "The name of the resource group"
-  value       = azurerm_resource_group.this.name
+  value       = azurerm_resource_group.rg.name
 }
 
 output "location" {
   description = "The location of the resource group"
-  value       = azurerm_resource_group.this.location
+  value       = azurerm_resource_group.rg.location
 }

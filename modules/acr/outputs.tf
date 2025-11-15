5395224a387d83115222ec827c6be27bea782d12
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2025-11-15 12:39:22 +0100

    Module outputs fixed

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -1,3 +1,8 @@
+output "id" {
+  description = "The ID of the container registry"
+  value       = azurerm_container_registry.acr.id
+}
+
 output "login_server" {
   description = "The login server of the container registry"
   value       = azurerm_container_registry.acr.login_server
